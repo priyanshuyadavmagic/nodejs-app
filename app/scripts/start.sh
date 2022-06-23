23c002528e84c4a883aca9ff205b24b55ca9af09
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2022-06-23 09:57:26 +0530

    Update start.sh

--- app/scripts/start.sh
+++ app/scripts/start.sh
@@ -2,8 +2,8 @@
 
 # killall -9 node || true
 # nohup /root/.nvm/versions/node/v10.11.0/bin/node /opt/apps/index.js > /opt/apps/start.log 2>&1 &
-npm install forever -g
 cd /opt/apps
+npm install forever -g
 node index.js
 killall node
 forever stopall
